919a5a06880176a615c759cba2d47dc6dcc19e46
sanikadongre <sanikadongre>
2018-10-07 16:19:55 -0600

    data received

--- hw_4/sensor_data.js
+++ hw_4/sensor_data.js
@@ -1,9 +1,9 @@
 var sensor = require('node-dht-sensor');
 
-sensor.read(22, 4, function(err, temperature, humidity) {
-    if (!err) {
-        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%'
-        );
-    }
-});
+sensor.read(22, 4, function(err,temperature, humidity) {
+	if(!err)
+	{
+	 console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%');
+	}
+    });
