542d96fb2e0ac24020d6a31e9cce5c9bb379a315
Sanika Dongre <sado9543@colorado.edu>
2018-10-13 05:15:30 -0600

    Update client.js

--- project2/client/client.js
+++ project2/client/client.js
@@ -12,4 +12,17 @@ $(document).ready(function () {
   var mintemp;
   var celsiusunit; 
   var faranunit;
- 
+ ws = new WebSocket("ws://10.0.0.241:8888/ws")
+
+//Error handling for websockets
+  ws.onerror = function(error){
+    alert("Cannot connect to server. Make sure server is online")
+  }
+ ws.onclose = function(evt) {
+      alert("Retry Refreshing the Webpage!");
+    }
+//handling Messages from tornado server
+  ws.onmessage = function(evt) {
+  // log("Message Received: " + evt.data)
+  //alert("message received: " + evt.data)
+    var str_arry = evt.data.split(" ")
