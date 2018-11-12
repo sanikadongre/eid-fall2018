05d7d2a12ce0ef0fd0a59565186754852547e99c
sanikadongre <sado9543@colorado.edu>
2018-11-12 05:19:41 -0700

    adding

--- project3/lambda.js
+++ project3/lambda.js
@@ -1,4 +1,4 @@
-//Initializations
+//Initialization
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'REGION'});
 var sqs = new AWS.SQS();
@@ -8,17 +8,19 @@ var max_temp = 0.00;
 var max_humid = 0.00;
 var min_temp = 100.00;
 var min_humid = 100.00;
-var count_temp = 1.0;
-var count_humid = 1.0;
+var count_temp = 0.00;
+var count_humid = 0.00;
+var prev_temp = 0.00;
+var prev_humid = 0.00;
 
-//Handler for event detection
+//Event handler
 exports.handler = (event, context, callback) => {
     var eventText = JSON.stringify(event, null, 2);
     console.log("Received event:", eventText);
-    // Taking values received from the IoT topic
+    //Get values
     var curr_temp = parseFloat(event.Temperature);
     var curr_humid = parseFloat(event.Humidity);
-    //Finding max,min,avg values
+    //Finding min, max, avg
     if(curr_temp>0)
     {
         avg_temp=((avg_temp*count_temp)+curr_temp)/(count_temp+1);
@@ -28,6 +30,11 @@ exports.handler = (event, context, callback) => {
         avg_temp = avg_temp.toFixed(2);
         max_temp = max_temp.toFixed(2);
         min_temp = min_temp.toFixed(2);
+        prev_temp=curr_temp;
+        if(!(curr_humid>0))
+        {
+            curr_humid=prev_humid;
+        }
     } 
     if(curr_humid>0)
     {
@@ -38,9 +45,14 @@ exports.handler = (event, context, callback) => {
         avg_humid = avg_humid.toFixed(2);
         max_humid = max_humid.toFixed(2);
         min_humid = min_humid.toFixed(2);
-    } 
+        prev_humid=curr_humid;
+        if(!(curr_temp>0))
+        {
+            curr_temp=prev_temp;
+        }
+    }
     
-    //Message to be written JSON format
+    //SQS JSON format
     var params = {
      DelaySeconds: 0,
      MessageBody: "{ \"curr_temp\": " + curr_temp +", " + " \"avg_temp\": " + avg_temp + "," + "\"max_temp\": " + max_temp + "," + "\"min_temp\": " + min_temp + "," +"\"curr_humid\": " + curr_humid + "," + "\"avg_humid\": " + avg_humid + "," + "\"max_humid\": " + max_humid + "," + "\"min_humid\": " + min_humid + "}",
@@ -55,7 +67,7 @@ exports.handler = (event, context, callback) => {
       console.log('data:',data.MessageId);
     }
 });
-    //Value logging for cloudwatch
+    //sending values in cloudwatch
     console.log(max_temp, min_temp, max_humid, min_humid, avg_temp, avg_humid, count_temp, count_humid);
     callback(null, curr_humid);
 };
