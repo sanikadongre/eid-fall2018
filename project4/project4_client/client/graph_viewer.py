8b47c8ce82ebf226f046e8640f5c1e6322bc2f87
sanikadongre <sado9543@colorado.edu>
2018-12-09 22:14:32 -0700

    client

--- project4/project4_client/client/graph_viewer.py
+++ project4/project4_client/client/graph_viewer.py
@@ -3,14 +3,15 @@
 #Reference links: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/sqs.html
 #sqs: https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_ReceiveMessage.html
 import datetime
-from websocket import create_connection
+from websocket import create_connection #for websockets
 import matplotlib.pyplot as plt
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt #for mqtt
 import boto3
 import ast
-from aiocoap import *
+from aiocoap import * #for coap
 import matplotlib
 import json
+import pika #for rabbitmq
 import threading
 import sys
 import asyncio
@@ -39,6 +40,8 @@ class Ui_MainWindow(object):
         self.coap_time=[]
         self.mqtt_time=[]
         self.message_num=[]
+        self.rabbitmq_time=[]
+
         
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -89,7 +92,8 @@ class Ui_MainWindow(object):
         self.Close.setText(_translate("MainWindow", "Exit"))
         self.getdata.setText(_translate("MainWindow", "Graph view"))
         self.getdata_2.setText(_translate("MainWindow", "Protocols"))
-            
+        
+    #function to get data       
     def get_data(self):
         queue_list = []
         for i in range(3):
@@ -128,15 +132,8 @@ class Ui_MainWindow(object):
         else:
             self.MessageBox.setText("Error Fetching Data \n")
             
-    def time_protocols(self):
+    def time_protocols(self): #function to calculate the execution time for protocols
         self.get_data()
-        web_time1 = time.time()
-        self.web_client()
-        web_time2 = time.time()
-        web_execution_time = web_time2 - web_time1
-        self.websockets_time.append(web_execution_time)
-        print('\nThe exchange time for Websocket is: %s'% web_execution_time)
-        print('\nnumber messages: %d'% self.readings)
         print("\nThe CoAP output:\n")
         coapthread = threading.Thread(target=self.coap_collect)
         coap_time1 = time.time()
@@ -149,23 +146,36 @@ class Ui_MainWindow(object):
         print('\nnumber of messages: %d'% self.readings)
         mqtt_time1 = time.time()
         client.publish(up_topic, self.output)
-        msg_event.wait()
+        mqtt_msgevent.wait()
         mqtt_time2 = time.time()
         mqtt_execution_time = (mqtt_time2 - mqtt_time1)
-        msg_event.clear()
+        mqtt_msgevent.clear()
         self.mqtt_time.append(mqtt_execution_time)
         print('\nThe exchange time for MQTT is: %s seconds'% mqtt_execution_time)
         print('\nnumber of messages: %d'% self.readings)
+        web_time1 = time.time()
+        self.web_client()
+        web_time2 = time.time()
+        web_execution_time = web_time2 - web_time1
+        self.websockets_time.append(web_execution_time)
+        print('\nThe exchange time for Websocket is: %s'% web_execution_time)
+        print('\nnumber messages: %d'% self.readings)
+        rabbit_time1 = time.time()
+        self.rabbitmq_publish()
+        rabbit_time2 = time.time()
+        rabbit_execution_time = rabbit_time2 - rabbit_time1
+        self.rabbitmq_time.append(rabbit_execution_time)
+        print('The exchange time for RabbitMQ is: %s'% rabbit_execution_time)
+        print('\nnumber messages: %d'% self.readings)
         self.message_num.append(self.readings)
         self.plotoutputprotocol()
-
-    
+  
     # Function for plotting graph of humidity and temperature separately
     def plotGraph(self):
         self.get_data()
         plt.plot(range(self.readings), self.max_temperature_data, 'r-', label='Max Temp')  #red
-        plt.plot(range(self.readings), self.min_temperature_data, 'b-', label='Min Temp')  #blue
-        plt.plot(range(self.readings), self.present_temperature_data, 'g-', label='Last Temp')  #green
+        plt.plot(range(self.readings), self.min_temperature_data, 'c-', label='Min Temp')  #blue
+        plt.plot(range(self.readings), self.present_temperature_data, 'm-', label='Last Temp')  #green
         plt.plot(range(self.readings), self.avg_temperature_data, 'y-', label='Avg Temp')  #yellow
         plt.legend(loc='best')
         plt.title('Temperature Graph')
@@ -173,8 +183,8 @@ class Ui_MainWindow(object):
         plt.xlabel('readings')
         plt.show()
         plt.plot(range(self.readings), self.max_humidity_data, 'r-', label='Max Hum')  #red
-        plt.plot(range(self.readings), self.min_humidity_data, 'b-', label='Min Hum')  #blue
-        plt.plot(range(self.readings), self.present_humidity_data, 'g-', label='Last Hum') #green
+        plt.plot(range(self.readings), self.min_humidity_data, 'c-', label='Min Hum')  #blue
+        plt.plot(range(self.readings), self.present_humidity_data, 'm-', label='Last Hum') #green
         plt.plot(range(self.readings), self.avg_humidity_data, 'y-', label='Avg Hum') #yellow
         plt.legend(loc='best')
         plt.title('Humidity Graph')
@@ -182,23 +192,18 @@ class Ui_MainWindow(object):
         plt.xlabel('Number of readings')
         plt.show()
         
-    def plotoutputprotocol(self):
-        plt.plot(self.message_num, self.websockets_time, 'g-', label='P1: WebSocket')
-        plt.plot(self.message_num, self.coap_time, 'r-', label='P2: CoAP')
-        plt.plot(self.message_num, self.mqtt_time, 'b-', label='P3: MQTT')
-        #plt.plot(self.num_messages_list, self.amqp_time_list, 'g-', label='Rabbit AMQP')
+    def plotoutputprotocol(self): #funcion for plotting the protocols
+        plt.plot(self.message_num, self.mqtt_time, 'g-', label='P1: MQTT')
+        plt.plot(self.message_num, self.coap_time, 'y-', label='P2: CoAP')
+        plt.plot(self.message_num, self.websockets_time, 'r-', label='P3: Websockets')
+        plt.plot(self.message_num, self.rabbitmq_time, 'm-', label='P4: RabbitMQ')
         plt.legend(loc='best')
         plt.title('Protocol comparison')
         plt.ylabel('Transfer time of protocol')
         plt.xlabel('Messages number')
         plt.show()
-        
-    def web_client(self):
-        ws.send(self.output)
-        websocket_output =  ws.recv()
-        print("\nwebsocket data:\n")
-        print(websocket_output)
-        
+    
+    #function for coapPut
     async def coapPUT(self, data):
         context = await Context.create_client_context()
         await asyncio.sleep
@@ -214,34 +219,62 @@ class Ui_MainWindow(object):
         loop_func = asyncio.get_event_loop()
         loop_func.run_until_complete(self.coapPUT(self.output))
         return 0
+        
+    def web_client(self): #websocket client
+        ws.send(self.output)
+        websocket_output =  ws.recv()
+        print("\nwebsocket data:\n")
+        print(websocket_output)
     
-def func_mqtt():
+    def rabbitmq_publish(self): #rabbitmq publish 
+        channel.queue_declare(queue='up_queue')
+        channel.basic_publish(exchange='', routing_key='up_queue', body= self.output)
+        return 0
+
+def rabbitmq_subscribe(): #rabbitmq subscribe
+    channel.queue_declare(queue='down_queue')
+    channel.basic_consume(callback,queue='down_queue', no_ack=True)
+    channel.start_consuming()
+
+def callback(ch, method, properties, body):
+    print("\nRabbit AMQP Data:\n%r" % body)
+    rabbit_msgevent.set()
+            
+def func_mqtt(): #mqtt function
     client.on_connect = on_connect
     client.on_message = on_message
     client.loop_forever()
-
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe(down_topic)
 
-
 def on_message(client, userdata, msg):
     print(str(msg.payload))
-    msg_event.set()
+    mqtt_msgevent.set()
 
 if __name__ == "__main__":
     ws = create_connection("ws://10.0.0.203:8888/ws")
-    msg_event = threading.Event()
+    ip = "10.0.0.203" #ip_addr
+    mqtt_msgevent = threading.Event()
+    rabbit_msgevent =threading.Event()
+    credentials = pika.PlainCredentials('guest', 'guest')
+    parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
     up_topic = 'mqtt_upstream'
     down_topic = 'mqtt_downstream'
     client = mqtt.Client()
-    client.connect('test.mosquitto.org',1883,60)
-    calcthread = []
-    mqttthread = threading.Thread(target=func_mqtt)
+    client.connect('test.mosquitto.org',1883,60) #using mosquitto broker
+    calcthread = [] #threads for parallel execution
+    mqttthread = threading.Thread(target=func_mqtt) #mqtt thread
     calcthread.append(mqttthread)
     mqttthread.daemon = True
     mqttthread.start()
+    rabbitmqthread = threading.Thread(target=rabbitmq_subscribe)
+    calcthread.append(rabbitmqthread)
+    rabbitmqthread.daemon = True
+    rabbitmqthread.start() #rabbitmqthread
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
